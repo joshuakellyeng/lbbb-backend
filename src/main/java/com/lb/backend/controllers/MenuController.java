d205a804510450a7bfde92551fb90d289a75b882
Joshua Kelly <joshuakellyeng@gmail.com>
2022-03-09 23:37:00 -0500

    updated CORS cross origin link

--- src/main/java/com/lb/backend/controllers/MenuController.java
+++ src/main/java/com/lb/backend/controllers/MenuController.java
@@ -22,7 +22,7 @@ import com.lb.backend.repositories.MenuItemCategoryRepository;
 import com.lb.backend.repositories.MenuItemsRepository;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://lb-burgerbar.netlify.app")
 @RequestMapping("/api/v1/")
 public class MenuController {
 	
